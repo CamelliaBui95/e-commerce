4fefe3845d92a03b713c08664216d5cfc6242aa4
CamelliaBui95 <camellia.bui95@gmail.com>
2026-08-15 16:18:18 +0200

    feat: add shadnc navBar

--- ecommerce-frontend/src/pages/components/Header.tsx
+++ ecommerce-frontend/src/pages/components/Header.tsx
@@ -1,15 +1,36 @@
-import React from "react";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from "@/components/ui/navigation-menu";
 import { Link } from "react-router";
 
 const Header = () => {
   return (
-    <div className="header p-4 flex flex-row w-full">
-      <div className="w-1/4">Ecommerce</div>
-      <div className="nav-bar w-3/4 flex flex-row justify-end-safe gap-8">
-        <Link to={"/"}>Home</Link>
-        <Link to={""}>About</Link>
-        <Link to={"/products"}>Products</Link>
-      </div>
+    <div className="header sticky top-0 z-50 bg-background p-4 flex flex-row items-center w-full">
+      <div className="logo text-xl font-bold text-pink-800">My E-commerce</div>
+
+      <NavigationMenu className="ml-auto">
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <NavigationMenuLink render={<Link to="/" />}>
+              Home
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+
+          <NavigationMenuItem>
+            <NavigationMenuTrigger>Products</NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <NavigationMenuLink render={<Link to="/products" />}>
+                Link
+              </NavigationMenuLink>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
     </div>
   );
 };
