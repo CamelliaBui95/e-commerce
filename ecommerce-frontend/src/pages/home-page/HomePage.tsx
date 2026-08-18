0d631589e555492d03360243f8fdab0115d0ba35
CamelliaBui95 <camellia.bui95@gmail.com>
2026-08-18 11:17:03 +0200

    fix: add new color palette + adjust position of hero-products

--- ecommerce-frontend/src/pages/home-page/HomePage.tsx
+++ ecommerce-frontend/src/pages/home-page/HomePage.tsx
@@ -4,21 +4,23 @@ import "./homePage.scss";
 const HomePage = () => {
   return (
     <div className="h-screen">
-      <section className="hero-section bg-light-vanilla-cream w-screen h-3/4 flex flex-row items-center justify-between">
+      <section className="hero-section bg-soft-ivory w-screen h-3/4 flex flex-row items-center justify-between overflow-hidden">
         <div className="hero-content">
           <p>
-            <span className="text-dark-brown font-bold bg-light-orange rounded-md py-1 px-3">
+            <span className="text-dark-brown font-bold bg-warm-cream rounded-md py-1 px-3">
               New Season
             </span>
           </p>
-          <h1 className="text-6xl font-bold">
+          <h1 className="text-6xl font-bold text-charcoal">
             Discover products that elevate your everyday
           </h1>
-          <p>Curated fashion, home essentials, and more - all in one place.</p>
+          <p className="text-xl text-dark-brown">
+            Curated fashion, home essentials, and more - all in one place.
+          </p>
         </div>
 
         <img
-          className="h-full"
+          className="h-145 self-end translate-x-15 translate-y-20"
           src="/images/hero-products.png"
           alt="hero-products"
         />
