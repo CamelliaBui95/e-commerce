a331987315c65bf704a83a566a89c14bd5289d45
CamelliaBui95 <camellia.bui95@gmail.com>
2026-09-01 20:00:44 +0200

    feat: search products by category

--- ecommerce-frontend/src/pages/ProductsPage.tsx
+++ ecommerce-frontend/src/pages/ProductsPage.tsx
@@ -1,17 +1,50 @@
+import { Category } from "@/enums/category";
 import { SortDirection } from "@/enums/sortDirection";
 import { addToCart } from "@/features/cart/cartSlice";
 import ProductCard from "@/features/product/ProductCard";
 import { useSearchProducts } from "@/hooks/useProducts";
+import { cn } from "@/lib/utils";
 import type { OrderItem } from "@/models/order";
 import type { Product } from "@/models/product";
-import React from "react";
+import { Cable, Ellipsis, Handbag, Lamp, Shapes, Shirt } from "lucide-react";
+import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 
+const CATEGORIES = [
+  {
+    category: Category.HOME,
+    icon: <Lamp strokeWidth={1} />,
+  },
+  {
+    category: Category.CLOTHING,
+    icon: <Shirt strokeWidth={1} />,
+  },
+  {
+    category: Category.ACCESSORIES,
+    icon: <Handbag strokeWidth={1} />,
+  },
+  {
+    category: Category.TOYS,
+    icon: <Shapes strokeWidth={1} />,
+  },
+  {
+    category: Category.ELECTRONICS,
+    icon: <Cable strokeWidth={1} />,
+  },
+  {
+    category: Category.OTHER,
+    icon: <Ellipsis strokeWidth={1} />,
+  },
+];
+
 const Products = () => {
+  const [category, setCategory] = useState<Category>(null);
+
   const { data } = useSearchProducts({
     pageNumber: 0,
     pageSize: 10,
     direction: SortDirection.DESC,
+    category: category,
   });
 
   const dispatch = useDispatch();
@@ -33,17 +66,42 @@ const Products = () => {
       <h2 className="text-center text-3xl font-bold font-accent">
         Discover Our Products
       </h2>
-
-      <div className="border-2 rounded-md p-8 grid gap-4 sm:grid-cols-2 lg:grid-cols-4">
-        {data?.content.map((product) => (
-          <ProductCard
-            key={product.id}
-            product={product}
-            currency="euro"
-            onAddToCart={handleAddToCart}
-          />
+      <ul className="flex flex-row gap-10">
+        {CATEGORIES.map((cat) => (
+          <li
+            key={cat.category}
+            className=" flex flex-row gap-2 hover:cursor-pointer"
+            onClick={() => setCategory(cat.category)}
+          >
+            {cat.icon}
+            <span
+              className={cn(
+                "relative after:absolute after:left-0 after:-bottom-1 after:h-[1px] after:w-0 after:bg-current after:transition-all after:duration-300 hover:after:w-full",
+                `${category === cat.category && "after:w-full"}`
+              )}
+            >
+              {cat.category}
+            </span>
+          </li>
         ))}
-      </div>
+      </ul>
+
+      {data?.content?.length > 0 ? (
+        <div className="border-2 rounded-md p-8 grid gap-4 sm:grid-cols-2 lg:grid-cols-4">
+          {data?.content.map((product) => (
+            <ProductCard
+              key={product.id}
+              product={product}
+              currency="euro"
+              onAddToCart={handleAddToCart}
+            />
+          ))}
+        </div>
+      ) : (
+        <div>
+          <h3 className="text-xl">Nothing yet...</h3>
+        </div>
+      )}
     </div>
   );
 };
