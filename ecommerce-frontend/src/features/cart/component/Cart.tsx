ff80d5fb8762486b2be1d0e3b56358f640815c50
CamelliaBui95 <camellia.bui95@gmail.com>
2026-09-07 18:12:20 +0200

    fix: set max height and overflow-auto to items list

--- ecommerce-frontend/src/features/cart/component/Cart.tsx
+++ ecommerce-frontend/src/features/cart/component/Cart.tsx
@@ -41,7 +41,7 @@ const Cart = () => {
         <SheetHeader className="h-full">
           <SheetTitle className="font-bold">{`My Cart (${cartItemCount})`}</SheetTitle>
           <div className="flex w-full max-w-md flex-col gap-1 py-2 h-full">
-            <CartItems />
+            <CartItems className="max-h-8/10 overflow-auto" />
             <ul className="py-2">
               <li className="text-lg font-bold flex flex-row justify-between my-1">
                 <span>Total</span>
